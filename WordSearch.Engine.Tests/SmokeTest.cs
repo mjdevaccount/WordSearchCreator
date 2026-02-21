3d4c1ca06f9fe7fe2e5acfe1ff072cb9c579a48a
mattj <matt.jeffcoat@gmail.com>
2026-02-21 16:02:50 -0600

    Define initial model and create test to verify project compatability

--- WordSearch.Engine.Tests/SmokeTest.cs
+++ WordSearch.Engine.Tests/SmokeTest.cs
@@ -1,4 +1,5 @@
-﻿using Xunit;
+﻿using WordSearch.Engine.Models;
+using Xunit;
 
 namespace WordSearch.Engine.Tests
 {
@@ -8,6 +9,17 @@ namespace WordSearch.Engine.Tests
         public void CanRun()
         {
             Assert.True(true);
+        }
+
+        [Fact]
+        public void CanCreateGrid()
+        {
+            var grid = new WordSearchGrid(10);
+
+            Assert.Equal(10, grid.Size);
+            Assert.Equal(10, grid.Grid.GetLength(0));
+            Assert.Equal(10, grid.Grid.GetLength(1));
+            Assert.Empty(grid.IncludedWords);
         }
     }
 }
--- WordSearch.Engine/Models/WordSearchGrid.cs
+++ WordSearch.Engine/Models/WordSearchGrid.cs
@@ -1,13 +1,18 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Collections.Generic;
 
 namespace WordSearch.Engine.Models
 {
     public class WordSearchGrid
     {
-        
+        public int Size { get; }
+        public char[,] Grid { get; set; }
+        public List<IncludedWord> IncludedWords { get; set; }
+
+        public WordSearchGrid(int size)
+        {
+            Size = size;
+            Grid = new char[size, size];
+            IncludedWords = new List<IncludedWord>();
+        }
     }
 }
