814dc4fba68f199f93f0b78e1b8bb0dc41631974
mattj <matt.jeffcoat@gmail.com>
2026-02-21 20:47:41 -0600

    Add post actions to WordSearchController

--- WordSearch.Web/Controllers/WordSearchController.cs
+++ WordSearch.Web/Controllers/WordSearchController.cs
@@ -1,17 +1,50 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Mvc;
+﻿using System.Web.Mvc;
+using WordSearch.Engine.Logic;
+using WordSearch.Engine.Models;
 
 namespace WordSearch.Web.Controllers
 {
     public class WordSearchController : Controller
     {
-        // GET: WordSearch
+        private const string SessionKey = "WordSearchGenerator";
+
         public ActionResult Index()
         {
             return View();
+        }
+
+        [HttpPost]
+        public ActionResult CreateGrid(int size)
+        {
+            var grid = new WordSearchGrid(size);
+            var generator = new WordSearchGenerator(grid);
+            Session[SessionKey] = generator;
+
+            return PartialView("_Grid", generator.Grid);
+        }
+
+        [HttpPost]
+        public ActionResult AddWord(string word)
+        {
+            if (!(Session[SessionKey] is WordSearchGenerator generator))
+                return Json(new { success = false, message = "No grid created" });
+
+            var result = generator.AddWord(word);
+
+            if (result.Success)
+                return PartialView("_Grid", generator.Grid);
+
+            return Json(new { success = false, message = result.ErrorMessage });
+        }
+
+        [HttpPost]
+        public ActionResult Rebuild()
+        {
+            if (!(Session[SessionKey] is WordSearchGenerator generator))
+                return Json(new { success = false, message = "No grid created" });
+
+            generator.Rebuild();
+            return PartialView("_Grid", generator.Grid);
         }
     }
 }
