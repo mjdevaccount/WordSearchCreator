c576eecdb8f94e7ca7ea7f8ad96e8a55d40cc8a3
mattj <matt.jeffcoat@gmail.com>
2026-02-21 20:42:31 -0600

    Remove default Home and add wordsearch controller

---
+++ WordSearch.Web/Controllers/WordSearchController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WordSearch.Web.Controllers
+{
+    public class WordSearchController : Controller
+    {
+        // GET: WordSearch
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
